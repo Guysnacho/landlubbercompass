977b74289ee73db2f835507a4c987f1a8b1868ac
Guysnacho <guysnacho2@gmail.com>
2020-07-17 13:00:25 -0500

    Map is present

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <v-app id="inspire">
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>LandLubber Compass</v-toolbar-title>
+    </v-app-bar>
+
     <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
         <v-list-item link>
@@ -21,41 +26,56 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app color="indigo" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
     <v-main>
       <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col class="text-center">
-            <v-tooltip left>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
+        <l-map
+          style="height: 100%; width: 100%"
+          :zoom="zoom"
+          :center="center"
+          @update:zoom="zoomUpdated"
+          @update:center="centerUpdated"
+          @update:bounds="boundsUpdated"
+        >
+          <l-tile-layer :url="url"></l-tile-layer>
+        </l-map>
       </v-container>
     </v-main>
-    <v-footer color="indigo" app>
+    <v-footer color="primary" app>
       <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import { LMap, LTileLayer } from "vue2-leaflet";
 export default {
   props: {
     source: String
   },
 
   data: () => ({
-    drawer: null
-  })
+    drawer: null,
+    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    zoom: 3,
+    center: [10, 10],
+    bounds: null
+  }),
+
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+    boundsUpdated(bounds) {
+      this.bounds = bounds;
+    }
+  },
+
+  components: {
+    LMap,
+    LTileLayer
+  }
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,5 +2,12 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
-
-export default new Vuetify({});
+/* eslint-disable */
+export default new Vuetify({
+    theme: {
+        primary: "#F3EEE8",
+        secondary: "#79CBF6",
+        accent: "#C13B53",
+        warning: "#FC5554"
+    }
+});
